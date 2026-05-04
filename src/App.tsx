4af3c1dc5d3d3b2d1f8d56dc133bb7a3a5abdb63
Mary Korostina <masha.korostina@mail.ru>
2026-05-04 14:18:03 +0300

    chore: настройка eslint и vitest

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,11 @@
-import './App.css'
+import './App.css';
 
 function App() {
-
   return (
     <>
-        <h1>movie app!</h1>
+      <h1>movie app!</h1>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
