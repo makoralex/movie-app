1eeb020e458ff33c55d544057fef0941b6213007
Mary Korostina <masha.korostina@mail.ru>
2026-05-11 15:55:21 +0300

    chore: small adjustments

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,35 @@
 import './App.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import AppHeader from './components/AppHeader/AppHeader';
+import HomePage from './pages/HomePage';
+import MoviesPage from './pages/MoviesPage';
+import AuthPage from './pages/AuthPage';
+import ProfilePage from './pages/ProfilePage';
+import ProtectedRoute from './components/ProtectedRoute';
+import MoviePage from './pages/MoviePage';
 
 function App() {
   return (
-    <>
-      <h1>movie app!</h1>
-    </>
+    <BrowserRouter>
+      <AppHeader />
+
+      <div className="container">
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/movies" element={<MoviesPage />} />
+          <Route path="/movies/:id" element={<MoviePage />} />
+          <Route path="/auth" element={<AuthPage />} />
+          <Route
+            path="/profile"
+            element={
+              <ProtectedRoute>
+                <ProfilePage />
+              </ProtectedRoute>
+            }
+          />
+        </Routes>
+      </div>
+    </BrowserRouter>
   );
 }
 
